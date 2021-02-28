b589cb4660fbe26af095f254c4db7d16e2ebf97c
Yolosopher <nika.nishnianidze.10@gmail.com>
2021-03-01 03:39:05 +0400

    ZONE 1

--- assets/js/dist/homepage.js
+++ assets/js/dist/homepage.js
@@ -31,13 +31,52 @@ const homeMainSlider = new Swiper('.homeMain__slider', {
 const runningstringP = document.querySelector('.runningString__p')
 const setRunningStringSpeed = (el) => {
 	const width = el.clientWidth
-	const speed = +width / 4 * 39
-	console.log(speed)
+	const speed = (+width / 4) * 39
 	el.style.animationDuration = `${speed}ms`
 	setTimeout(() => {
 		el.classList.add('running')
-	}, 50);
+	}, 50)
 }
-window.addEventListener('load', () => {
+$(document).ready(function () {
 	setRunningStringSpeed(runningstringP)
+
+	$('#datepicker').simpleCalendar({
+		//Defaults options below
+		//string of months starting from january
+		months: [
+			'january',
+			'february',
+			'march',
+			'april',
+			'may',
+			'june',
+			'july',
+			'august',
+			'september',
+			'october',
+			'november',
+			'december',
+		],
+		days: [
+			'su',
+			'mo',
+			'tu',
+			'we',
+			'th',
+			'fr',
+			'sa',
+		],
+		displayYear: true, // Display year in header
+		fixedStartDay: 0, // Week begin always by monday or by day set by number 0 = sunday, 7 = saturday, false = month always begin by first day of the month
+		displayEvent: true, // Display existing event
+		disableEventDetails: true, // disable showing event details
+		disableEmptyDetails: true, // disable showing empty date details
+		events: [], // List of events
+		onInit: function (calendar) {}, // Callback after first initialization
+		onMonthChange: function (month, year) {}, // Callback on month change
+		onDateSelect: function (date, events) {}, // Callback on date selection
+		onEventSelect: function () {}, // Callback on event selection - use $(this).data('event') to access the event
+		onEventCreate: function ($el) {}, // Callback fired when an HTML event is created - see $(this).data('event')
+		onDayCreate: function ($el, d, m, y) {}, // Callback fired when an HTML day is created   - see $(this).data('today'), .data('todayEvents')
+	})
 })
